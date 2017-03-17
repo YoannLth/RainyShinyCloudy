c949c21639ef10eef51519919090615fec7161cd
YoannLTH <yoann.lathuiliere@gmail.com>
2017-03-17 10:00:05 +0100

    [WIP] UI

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  RainyShinyCloudy
+//
+//  Created by yoann lathuiliere on 17/03/2017.
+//  Copyright © 2017 yoann lathuiliere. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+  // MARK: - Outlets
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var tempLabel: UILabel!
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var weatherLabel: UILabel!
+  @IBOutlet weak var weatherImage: UIImageView!
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  
+  
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
